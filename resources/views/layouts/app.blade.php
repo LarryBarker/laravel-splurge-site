0613da30ee00b9e8dd26dc00f3015d0f2f38f5dc
Larry Barker <larry@Larrys-MBP.lan>
2020-06-20 18:11:37 -0500

    use gravatar for profile photo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,8 +52,8 @@
                         <div class="flex items-center">
                             <div>
                                 <img class="inline-block h-10 w-10 rounded-full"
-                                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80"
-                                    alt="">
+                                    src="{{ auth()->user()->avatarUrl() }}"
+                                    alt="Profile photo">
                             </div>
                             <div class="ml-3">
                                 <p class="text-base leading-6 font-medium text-white">
@@ -101,8 +101,8 @@
                     <div class="flex items-center">
                         <div>
                             <img class="inline-block h-9 w-9 rounded-full"
-                                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80"
-                                alt="">
+                                src="{{ auth()->user()->avatarUrl() }}"
+                                alt="Profile photo">
                         </div>
 
                         <div class="ml-3">
--- resources/views/livewire/profile.blade.php
+++ resources/views/livewire/profile.blade.php
@@ -19,9 +19,7 @@
                 <div class="mt-2 sm:mt-0 sm:col-span-2">
                     <div class="flex items-center">
                         <span class="h-12 w-12 rounded-full overflow-hidden bg-gray-100">
-                            <svg class="h-full w-full text-gray-300" fill="currentColor" viewBox="0 0 24 24">
-                                <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
-                            </svg>
+                            <img src="{{ auth()->user()->avatarUrl() }}" alt="Profile photo">
                         </span>
                         <span class="ml-5 rounded-md shadow-sm">
                             <button type="button" class="py-2 px-3 border border-gray-300 rounded-md text-sm leading-4 font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800 transition duration-150 ease-in-out">
