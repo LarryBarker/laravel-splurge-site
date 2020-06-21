2aff6c6e5710bf352711af2bc1ddc4a98aa9dc2a
Larry Barker <larry@Larrys-MBP.lan>
2020-06-20 20:41:49 -0500

    make file upload component

--- resources/views/livewire/profile.blade.php
+++ resources/views/livewire/profile.blade.php
@@ -18,26 +18,15 @@
             </x-input.group>
 
             <x-input.group label="Photo" for="photo" :error="$errors->first('newAvatar')">
-                <div class="flex items-center">
-
+                <x-input.file-upload wire:model="newAvatar" id="photo">
                     <span class="h-12 w-12 rounded-full overflow-hidden bg-gray-100">
                         @if ($newAvatar)
-                        <img src="{{ $newAvatar->temporaryUrl() }}" alt="Profile photo">
+                            <img src="{{ $newAvatar->temporaryUrl() }}" alt="Profile photo">
                         @else
-                        <img src="{{ auth()->user()->avatarUrl() }}" alt="Profile photo">
+                            <img src="{{ auth()->user()->avatarUrl() }}" alt="Profile photo">
                         @endif
                     </span>
-
-                    <div x-data="{ focused: false }">
-                        <span class="ml-5">
-                            <input @focus="focused = true" @blur="focused = false" class="sr-only" type="file" id="photo" wire:model="newAvatar" multiple>
-                            <label for="photo" :class="{ 'outline-none border-blue-300 shadow-outline-blue' : focused }" class="cursor-pointer py-2 px-3 border border-gray-300 rounded-md text-sm leading-4 font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800 transition duration-150 ease-in-out">
-                                Change
-                            </label>
-                        </span>
-                    </div>
-
-                </div>
+                </x-input.avatar>
             </x-input.group>
 
         </div>
